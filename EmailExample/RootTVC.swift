0bd55831453691957a4e9838a2ea8664b436cd1f
bmousa <bmousa@cpcom102macm35.care.asu.edu>
2017-09-27 15:44:07 -0700

    Emailhw

--- EmailExample/RootTVC.swift
+++ EmailExample/RootTVC.swift
@@ -86,6 +86,7 @@ class RootTVC: UITableViewController {
     
 
     
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
